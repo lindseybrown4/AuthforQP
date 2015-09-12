e0f2c461213ddd64ae15c1fbeddf8a97906e6bc4
lindseybrown4 <lindseybrown4@gmail.com>
2015-09-12 01:20:51 -0600

    initial commit

---
+++ public/app/app.js
@@ -0,0 +1,35 @@
+ var app = angular.module('QueuePlate', []);
+
+// app.config(function($httpProvider, $stateProvider, $urlRouterProvider, $locationProvider) {
+
+// $httpProvider.interceptors.push('AuthInterceptor');
+
+//  $urlRouterProvider.otherwise('/');
+
+//  $stateProvider
+//    .state('home', {
+//      url: '/',
+//      templateUrl: 'public/app/home/home.html',
+//    })
+
+//    .state('login', {
+//      url: '/login',
+//      templateUrl: 'public/app/auth/login.html',
+//      controller: 'loginCtrl'
+//    })
+
+//   .state('signup', {
+// 		url: '/signup',
+// 		templateUrl: 'public/app/auth/signup.html',
+// 		controller: 'sigunupCtrl'
+
+// 	})
+
+// .state('dashboard', {
+// 		url: '/dashboard',
+// 		templateUrl: 'public/app/dashboard/dashboard.html'
+// 	})
+
+// $locationProvider.html5Mode(true);
+
+// });
---
+++ public/app/auth/signupService.js
@@ -0,0 +1,3 @@
+var app = angular.module('QueuePlate')
+
+app.service('signupService')
